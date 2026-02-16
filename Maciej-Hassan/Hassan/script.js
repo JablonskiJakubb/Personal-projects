32e30b0f43f417ae92676df95001cd5429860876
Jacob Jablonski <Jakub.Jablonski281007@gmail.com>
2026-02-16 13:03:28 +0100

    Delete Maciej-Hassan/Hassan directory

--- Maciej-Hassan/Hassan/script.js
+++
@@ -1,8 +0,0 @@
-let index = 0;
-
-function moveSlide(step) {
-    const slides = document.querySelectorAll('.carousel-item');
-    const inner = document.querySelector('.carousel-inner');
-    index = (index + step + slides.length) % slides.length;
-    inner.style.transform = `translateX(${-index * 100}%)`;
-}
